e8c1d6b81b4582a1fe7d73fa86e1044dadb353ac
Fabien Terrani <fabien.terrani@heig-vd.ch>
2022-04-23 18:28:36 +0200

    Added basic prank config validation

--- src/main/java/config/PrankConfig.java
+++ src/main/java/config/PrankConfig.java
@@ -10,6 +10,34 @@ public class PrankConfig
 
     public PrankConfig( String smtpHost, int smtpPort, int nbPrankGroups, PrankMail[] prankMails, String[] victimsMails )
     {
+        // checking port number
+        if ( smtpPort <= 0 || smtpPort > 65535 )
+            throw new IllegalArgumentException("Invalid port number: " + smtpPort);
+
+        // checking compatibility between the number of groups and the number of victim e-mail addresses
+        int maxCreatableGroups = victimsMails.length / 3;
+        if ( maxCreatableGroups < nbPrankGroups )
+        {
+            throw new IllegalArgumentException(
+                String.format(
+                    "Incompatible number of groups and victim e-mail addresses: at most %d group(s) can be created" +
+                    " using the %d victim addresses whereas %d groups were asked for.",
+                    maxCreatableGroups, victimsMails.length, nbPrankGroups
+                )
+            );
+        }
+
+        // checking victim e-mail addresses
+        for ( String victimMail : victimsMails )
+        {
+            if ( ! victimMail.contains( "@" ) )
+            {
+                throw new IllegalArgumentException(
+                    String.format( "Invalid victim e-mail address: %s", victimMail )
+                );
+            }
+        }
+
         this.smtpHost = smtpHost;
         this.smtpPort = smtpPort;
         this.nbPrankGroups = nbPrankGroups;
