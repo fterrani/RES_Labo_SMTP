bd2a2c622573b4be5149b3a3d960dea5cdd6d28e
Kitsunemma <yecomte@gmail.com>
2022-05-02 19:52:42 +0200

    implement group random creation

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,9 +1,12 @@
 import config.PrankConfigReader;
 import config.PrankConfig;
+import config.PrankGroup;
 import smtp.SmtpClient;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Program
 {
@@ -20,7 +23,54 @@ public class Program
         PrankConfig config = PrankConfigReader.readPrankConfig( configPath );
         System.out.println();
         SmtpClient client = new SmtpClient(config.getSmtpHost(), config.getSmtpPort());
-        client.sendEmail(config.getVictimsMails()[0], config.getVictimsMails(), config.getPrankMails()[1].getSubject(), config.getPrankMails()[1].getBody());
+        PrankGroup[] groups = createGroups(config.getVictimsMails(), config.getNbPrankGroups());
+        Random rand = new Random();
+        int mailsIndex = rand.nextInt(config.getPrankMails().length);
+        for (PrankGroup group : groups) {
+            client.sendEmail(group.getSender(), group.getVictims(), config.getPrankMails()[mailsIndex].getSubject(), config.getPrankMails()[mailsIndex].getBody());
+        }
         client.quit();
     }
+     private static PrankGroup[] createGroups(String[] victims, int nbGroups) {
+         int groupsSize = victims.length / nbGroups;
+         int rest = victims.length % nbGroups;
+         System.out.println(groupsSize);
+         PrankGroup[] groups = new PrankGroup[nbGroups];
+         for (int i = 0; i < nbGroups; ++i) {
+             if (i < rest)
+                 groups[i] = new PrankGroup(groupsSize + 1);
+             else
+                 groups[i] = new PrankGroup(groupsSize);
+         }
+         Random rand = new Random();
+
+         int nbToAdd = victims.length;
+         int i;
+         String tmp;
+         for (PrankGroup group : groups) {
+             i  = rand.nextInt(nbToAdd);
+             group.setSender(victims[i]);
+
+             tmp = victims[nbToAdd - 1];
+             victims[nbToAdd - 1] = victims[i];
+             victims[i] = tmp;
+             --nbToAdd;
+             while (!group.isFull()) {
+                 i  = rand.nextInt(nbToAdd);
+                 group.addVictim(victims[i]);
+                 
+                 tmp = victims[nbToAdd - 1];
+                 victims[nbToAdd - 1] = victims[i];
+                 victims[i] = tmp;
+                 --nbToAdd;
+             }
+         }
+         return groups;
+     }
+
+     private static void swap(String a, String b) {
+         String tmp = a;
+         a = b;
+         b = tmp;
+     }
 }
